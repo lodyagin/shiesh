7d9ef0401195161a80851e81df33d8d12492d38d
serg <serg>
2010-02-11 23:13:19 +0000

    Make a thread hierarchy.

--- coressh/Options.cpp
+++ coressh/Options.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Options.h"
 
-const std::string Options::version ("1.1d9");
+const std::string Options::version ("1.1d10");
 const std::string Options::progName ("CoreSSH");
 
 const std::string 
