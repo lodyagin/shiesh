e5fad34c6676be851ae0b398a468ac35cd86ce4c
serg <serg>
2010-03-10 15:07:11 +0000

    Set the target windows version

--- Server/stdafx.h
+++ Server/stdafx.h
@@ -1,16 +1,11 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#include <stdio.h>
-#include <tchar.h>
+#include "targetver.h"
 
-#define _WIN32_WINNT 0x0400
-// TODO: reference additional headers your program requires here
+#define WIN32_LEAN_AND_MEAN // without winsock 1
+#include <windows.h>
+#include <Ws2tcpip.h>
+#include <Winsock2.h>
 
 #include "Logging.h"
 #include "SCheck.h"
@@ -19,4 +14,7 @@
 #include "SThread.h"
 #include "SException.h"
 #include "SShutdown.h"
+
 #include <iostream>
+#include <stdio.h>
+#include <tchar.h>
--- coressh/StdAfx.h
+++ coressh/StdAfx.h
@@ -1,21 +1,22 @@
 #pragma once
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN // without winsock 1
+#include <windows.h>
+#include <Ws2tcpip.h>
+#include <Winsock2.h>
+
 #include "defines.h"
 #include "ssh.h"
 #include "xmalloc.h"
 #include "log.h"
-//#include "CoreConnection.h"
 #include "SException.h"
 #include "Logging.h"
 #include "SCommon.h"
 #include "SWinCheck.h"
-#include <winsock2.h>
-#include <string>
-
-#include "Ws2tcpip.h" 
-
 #include "zlib.h"
-
 #include "port.h"
 
+#include <string>
 #include <openssl/ossl_typ.h>
 #include <assert.h>
